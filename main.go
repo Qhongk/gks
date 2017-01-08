3b64734252f695d3a700716ad30ce40c027a784e
qinghong.kqh <qinghong.kqh@alibaba-inc.com>
2017-01-08 16:16:42 +0800

    project files

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 "github.com/astaxie/beego"
 )
+//import (
+//	_ "quickstart/routers"
+//	"github.com/astaxie/beego"
+//)
 
 type MainController struct {
 	beego.Controller
 }
 
 func (this *MainController) Get() {
-	this.Ctx.WriteString("hello world")
+	//this.Ctx.WriteString("hello world")
+	this.Ctx.Redirect(200, "index")
 }
 
 func main() {
